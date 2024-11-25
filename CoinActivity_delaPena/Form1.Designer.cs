f61eb0d65726a8750616f58e2c14f978152adf51
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-26 02:14:41 +0800

    Initial commit

---
+++ CoinActivity_delaPena/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace CoinActivity_delaPena
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 306);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(464, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total Coins: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(567, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(464, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total 5 Peso Coin:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(464, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Total 1 Peso Coin:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(464, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Total 25 Centavo Coin";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(464, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Total 10 Centavo Coin";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(464, 273);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Total 5 Centavo Coin";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(464, 317);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Total amount: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(551, 58);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 16);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "None";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(596, 101);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "None";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(596, 140);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(40, 16);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "None";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(620, 181);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(40, 16);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "None";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(620, 227);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(40, 16);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "None";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(620, 273);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(40, 16);
+            this.label14.TabIndex = 15;
+            this.label14.Text = "None";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(567, 317);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(40, 16);
+            this.label15.TabIndex = 16;
+            this.label15.Text = "None";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 37);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Analyze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(399, 353);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(389, 306);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ CoinActivity_delaPena/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinActivity_delaPena
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loaded, processed;
+        int TotalCoins;
+        float TotalAmount;
+        int Total5P;
+        int Total1P;
+        int Total25C;
+        int Total10C;
+        int Total5C;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            loaded = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = loaded;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(loaded == null)
+            {
+                return;
+            }
+            BlackAndWhiteBinaryConversion();
+            if(processed == null)
+            {
+                return;
+            }
+            initializeTotals();
+            GetCoinPixels(processed,ref TotalCoins,ref TotalAmount,ref Total5P,ref Total1P,ref Total25C,ref Total10C,ref Total5C);
+            assignTotals();
+        }
+
+        private void BlackAndWhiteBinaryConversion()
+        {
+            if (loaded == null)
+                return;
+
+            Bitmap binaryImage = new Bitmap(loaded.Width, loaded.Height);
+            int threshold = 180;
+
+            for (int x = 0; x < loaded.Width; ++x)
+            {
+                for (int y = 0; y < loaded.Height; ++y)
+                {
+                    Color pixel = loaded.GetPixel(x, y);
+                    int grayscale = (pixel.R + pixel.G + pixel.B) / 3;
+                    binaryImage.SetPixel(x, y, grayscale < threshold ? Color.Black : Color.White);
+                }
+            }
+
+            processed = binaryImage;
+
+            pictureBox2.Image = processed;
+        }
+
+        public static void GetCoinPixels(Bitmap image,ref int totalCount,ref float totalValue,ref int peso5Count,ref int peso1Count,ref int cent25Count,ref int cent10Count,ref int cent5Count)
+        {
+            bool[,] seen = InitializeSeenMatrix(image.Width, image.Height);
+            List<int> regionSizes = IdentifyRegions(image, seen);
+
+            ProcessRegions(regionSizes,ref totalCount,ref totalValue,ref peso5Count,ref peso1Count,ref cent25Count,ref cent10Count,ref cent5Count);
+
+
+        }
+        private static bool[,] InitializeSeenMatrix(int width, int height)
+        {
+            return new bool[width, height];
+        }
+
+        private static List<int> IdentifyRegions(Bitmap image, bool[,] seen)
+        {
+            List<int> regionSizes = new List<int>();
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (seen[x, y] || image.GetPixel(x, y).R != 0)
+                        continue;
+
+                    int regionSize = CalculateRegionSize(image, x, y, seen);
+
+                    if (regionSize >= 800)
+                    {
+                        regionSizes.Add(regionSize);
+                    }
+                }
+            }
+
+            return regionSizes;
+        }
+
+        private static int CalculateRegionSize(Bitmap image, int startX, int startY, bool[,] visited)
+        {
+            Stack<(int x, int y)> stack = new Stack<(int x, int y)>();
+            stack.Push((startX, startY));
+
+            int regionSize = 0;
+
+            while (stack.Count > 0)
+            {
+                var (x, y) = stack.Pop();
+                if (x < 0 || y < 0 || x >= image.Width || y >= image.Height || visited[x, y])
+                    continue;
+                visited[x, y] = true;
+
+                if (image.GetPixel(x, y).R != 0)
+                    continue;
+
+                regionSize++;
+
+                stack.Push((x - 1, y));
+                stack.Push((x + 1, y));
+                stack.Push((x, y - 1));
+                stack.Push((x, y + 1));
+            }
+
+            return regionSize;
+        }
+
+        private static void ProcessRegions(List<int> regionSizes,ref int totalCount,ref float totalValue,ref int peso5Count,ref int peso1Count,ref int cent25Count,ref int cent10Count,ref int cent5Count)
+        {
+            int localPeso5Count = peso5Count;
+            int localPeso1Count = peso1Count;
+            int localCent25Count = cent25Count;
+            int localCent10Count = cent10Count;
+            int localCent5Count = cent5Count;
+
+            
+            var coinClassification = new List<(int MinSize, float Value, Action)>
+            {
+                (6800, 5.0f, new Action(() => localPeso5Count++)),   
+                (4800, 1.0f, new Action(() => localPeso1Count++)),  
+                (3800, 0.25f, new Action(() => localCent25Count++)),
+                (2800, 0.10f, new Action(() => localCent10Count++)),
+                (800, 0.05f, new Action(() => localCent5Count++))   
+            };
+
+            foreach (int size in regionSizes)
+            {
+                foreach (var (MinSize, Value, Increment) in coinClassification)
+                {
+                    if (size > MinSize)
+                    {
+                        Increment();             
+                        totalValue += Value;     
+                        totalCount++;  
+                        break;                   
+                    }
+                }
+            }
+
+            
+            peso5Count = localPeso5Count;
+            peso1Count = localPeso1Count;
+            cent25Count = localCent25Count;
+            cent10Count = localCent10Count;
+            cent5Count = localCent5Count;
+
+        }
+        private void assignTotals()
+        {
+            label9.Text = TotalCoins.ToString();
+            label10.Text = Total5P.ToString();
+            label11.Text = Total1P.ToString();
+            label12.Text = Total25C.ToString();
+            label13.Text = Total10C.ToString();
+            label14.Text = Total5C.ToString();
+            label15.Text = TotalAmount.ToString();
+        }
+        private void initializeTotals()
+        {
+            TotalCoins = 0;
+            Total5P = 0;
+            Total1P = 0;
+            Total25C = 0;
+            Total10C= 0;
+            Total5C = 0;
+            TotalAmount = 0;
+        }
+    }
+}
